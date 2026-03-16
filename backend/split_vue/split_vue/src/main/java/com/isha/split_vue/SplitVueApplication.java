f3fe045e29c8deba4152aba05699c07c88693f33
Ishabam <ishabam09@gmail.com>
2026-03-16 16:30:54 +0530

    Initial project setup - Spring Boot + React + MySQL

---
+++ backend/split_vue/split_vue/src/main/java/com/isha/split_vue/SplitVueApplication.java
@@ -0,0 +1,13 @@
+package com.isha.split_vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitVueApplication.class, args);
+	}
+
+}
---
+++ backend/split_vue/split_vue/src/test/java/com/isha/split_vue/SplitVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isha.split_vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
